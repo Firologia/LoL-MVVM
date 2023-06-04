41168145e5f4fff4b03c6fb0feee8be8cd052934
Firologia <jordan.artzet1@gmail.com>
2023-06-04 15:18:39 +0200

    :sparkles: add SkinVM and update view

---
+++ Sources/LoL-MVVM/ViewModel/SkinVM.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Model;
+
+namespace ViewModel;
+
+public class SkinVM : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private Skin _model;
+
+    public string Name => _model.Name;
+
+    public string Description
+    {
+        get => _model.Description;
+        set
+        {
+            if(_model.Description.Equals(value)) return;
+            _model.Description = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Icon
+    {
+        get => _model.Icon;
+        set
+        {
+            if(_model.Icon.Equals(value)) return;
+            _model.Icon = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public float Price
+    {
+        get => _model.Price;
+        set
+        {
+            if(_model.Price.Equals(value)) return;
+            _model.Price = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public SkinVM(Skin model)
+    {
+        _model = model;
+    }
+}
